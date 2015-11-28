1f03921cb7b95ca2ce3c0d75743cf4d5ef080d40
Thad House <thadhouse1@gmail.com>
2015-11-27 19:13:06 -0800

    Initial Commit of tests

---
+++ NotifierIssue/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPILib;
+
+namespace NotifierIssue
+{
+    public class NotifierTest : IterativeRobot
+    {
+        Notifier notifier;
+
+        public override void RobotInit()
+        {
+            DateTime old = DateTime.Now;
+
+            notifier = new Notifier(() =>
+            {
+                DateTime current = DateTime.Now;
+                var delta = current - old;
+                Console.WriteLine(delta);
+                old = current;
+            });
+
+            notifier.StartPeriodic(0.1);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RobotBase.Main(null, typeof(NotifierTest));
+        }
+    }
+}
---
+++ StatusCheckIssue/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using HAL_Base;
+using WPILib;
+
+namespace StatusCheckIssue
+{
+    public class StatusCheckTest : IterativeRobot
+    {
+        public override void RobotInit()
+        {
+            int status = 0;
+            uint value = HAL.GetFPGARevision(ref status);
+            Utility.CheckStatus(status);
+            Console.WriteLine(value);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RobotBase.Main(null, typeof(StatusCheckTest));
+        }
+    }
+}
